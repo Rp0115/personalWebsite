5aff12830854798aa21d404d1c26ad4faa19c978
Rp0115 <riju0115@gmail.com>
2026-03-28 02:30:17 -0400

    first deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/** Repo: https://github.com/Rp0115/personalWebsite → https://rp0115.github.io/personalWebsite/ */
 export default defineConfig({
+  base: '/personalWebsite/',
   plugins: [react()],
 })
